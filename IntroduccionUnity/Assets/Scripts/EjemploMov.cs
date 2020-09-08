4e669d4609a6c693f926ad6af9448c1fb1c0d7e2
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-09-08 14:13:11 -0500

    Primer proyecto de Unity

---
+++ IntroduccionUnity/Assets/Scripts/EjemploMov.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploMov : MonoBehaviour
+{
+    public Vector3 goal = new Vector3(3,3,0);
+    public GameObject target;
+    public float speed = 0.1f;
+    void Start()
+    {
+       this.transform.Translate(goal); 
+       if(target == null) { 
+            Debug.LogError("No se encontró ningún objeto");
+       }
+    }
+
+    void Update()
+    {
+        Vector3 delta = target.transform.position - this.transform.position;
+
+        float dist = Vector3.Distance(target.transform.position,this.transform.position);
+        
+        if(dist > 0.1f) { 
+            this.transform.Translate(speed * delta.normalized);
+        } 
+    }
+}
---
+++ IntroduccionUnity/Assets/Scripts/EjemploRigibody.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploRigibody : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");    
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit");    
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("OnColissionStay");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter");
+        var renderer = other.gameObject.GetComponent<Renderer>();
+        renderer.material.color = Color.red;
+        var trash = other.gameObject.GetComponent<Trash>();
+
+        if(trash != null) { 
+            Destroy(other.gameObject,2);
+        }
+//        Debug.DrawLine(this.transform.position,other.gameObject.transform.position,Color.red, 10);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.DrawLine(this.transform.position,other.gameObject.transform.position,Color.red, 0.1f);
+        //Debug.Log("OnTriggerStay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //var renderer = other.gameObject.GetComponent<Renderer>();
+        //renderer.material.color = Color.white;
+        //Debug.Log("OnTriggerExit");
+    }
+}
