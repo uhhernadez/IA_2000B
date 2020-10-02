ffe6061dfd5b7bd6a001e0b29c02ecd909c88369
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-10-02 15:43:16 -0500

    Cambios en el juego

---
+++ ProblemasBusqueda/Assets/Scripts/TicTacToeAccion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TicTacToeAccion : MonoBehaviour, IPointerClickHandler
+{
+    public TicTacToeTablero tablero;
+    public int posicion;
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        tablero.Mover(posicion);
+        Debug.Log("Click en la posición: " + posicion);
+    }
+}
--- ProblemasBusqueda/Assets/Scripts/TicTacToeTablero.cs
+++ ProblemasBusqueda/Assets/Scripts/TicTacToeTablero.cs
@@ -8,10 +8,32 @@ public class TicTacToeTablero : MonoBehaviour
     // Arreglo para almacenar las posiciones de las piezas
     public GameObject [] posiciones = new GameObject[9];
 
+    public GameObject piezaX;
+    public GameObject piezaO;
+
+    private int turno = 0;
+
     public bool Mover(int posicion)
     { 
+        if(piezas[posicion] != null ) 
+        { 
+            Debug.LogWarning("El espacio ya está ocupado");
+            return false;
+        }
     
-        return false;
+        if(turno%2 == 0) 
+        {   
+            piezas[posicion] = Instantiate(piezaX);
+        } 
+        else
+        { 
+            piezas[posicion] = Instantiate(piezaO);
+        }
+
+        piezas[posicion].transform.position = posiciones[posicion].transform.position;
+        piezas[posicion].transform.parent = this.transform;
+        turno++;
+        return true;
     }
 
     public bool ExisteUnGanador()
