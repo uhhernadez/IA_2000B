33650089a188e6ba7113d6ecdbaeab3376dac673
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-10-02 15:04:05 -0500

    Cambios de la seleccion

---
+++ ProblemasBusqueda/Assets/Scripts/TicTacToeTablero.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class TicTacToeTablero : MonoBehaviour
+{
+    // Arreglo para almacenar las piezas de los jugadores.
+    public GameObject [] piezas = new GameObject[9];
+
+    // Arreglo para almacenar las posiciones de las piezas
+    public GameObject [] posiciones = new GameObject[9];
+
+    public bool Mover(int posicion)
+    { 
+    
+        return false;
+    }
+
+    public bool ExisteUnGanador()
+    { 
+    
+        return false;
+    }
+
+
+    void dummy() 
+    { 
+        for(int k = 0; k < piezas.Length; k++) 
+        { 
+            if(piezas[k] == null) 
+            { 
+                
+            }
+        }
+    
+    }
+
+}
