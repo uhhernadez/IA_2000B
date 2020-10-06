48794caabd1ca51864f42fd4282c753b19edb8cf
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-10-06 15:31:23 -0500

    Segundo posible enfoque de solución

--- ProblemasBusqueda/Assets/Scripts/TicTacToeTablero.cs
+++ ProblemasBusqueda/Assets/Scripts/TicTacToeTablero.cs
@@ -15,6 +15,11 @@ public class TicTacToeTablero : MonoBehaviour
 
     public bool Mover(int posicion)
     { 
+        if(ExisteUnGanador()) 
+        { 
+            Debug.Log("Ya hubo un ganador");
+            return false; 
+        }
         if(piezas[posicion] != null ) 
         { 
             Debug.LogWarning("El espacio ya está ocupado");
@@ -30,6 +35,7 @@ public class TicTacToeTablero : MonoBehaviour
             piezas[posicion] = Instantiate(piezaO);
         }
 
+        Debug.Log("Tipo de pieza " + piezas[posicion].GetComponent<Pieza>().estado);
         piezas[posicion].transform.position = posiciones[posicion].transform.position;
         piezas[posicion].transform.parent = this.transform;
         turno++;
@@ -38,10 +44,115 @@ public class TicTacToeTablero : MonoBehaviour
 
     public bool ExisteUnGanador()
     { 
-    
+        if(TresEnLinea("O")) 
+        {
+            return true;
+        }
+
+        if(TresEnLinea("X"))
+        { 
+            return true;
+        }
+
         return false;
     }
 
+    public bool TresEnLinea(string pieza) 
+    { 
+        if(piezas[0] != null && piezas[1] != null && piezas[2] != null) 
+        {  
+            if(piezas[0].GetComponent<Pieza>().estado == pieza && 
+               piezas[1].GetComponent<Pieza>().estado == pieza && 
+               piezas[2].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+
+        if(piezas[3] != null && piezas[4] != null && piezas[5] != null) 
+        {  
+            if(piezas[3].GetComponent<Pieza>().estado == pieza && 
+               piezas[4].GetComponent<Pieza>().estado == pieza && 
+               piezas[5].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+        if(piezas[6] != null && piezas[7] != null && piezas[8] != null) 
+        {  
+            if(piezas[6].GetComponent<Pieza>().estado == pieza && 
+               piezas[7].GetComponent<Pieza>().estado == pieza && 
+               piezas[8].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+
+        // Verticales
+        if(piezas[0] != null && piezas[3] != null && piezas[6] != null) 
+        {  
+            if(piezas[0].GetComponent<Pieza>().estado == pieza && 
+               piezas[3].GetComponent<Pieza>().estado == pieza && 
+               piezas[6].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+        if(piezas[1] != null && piezas[4] != null && piezas[7] != null) 
+        {  
+            if(piezas[1].GetComponent<Pieza>().estado == pieza && 
+               piezas[4].GetComponent<Pieza>().estado == pieza && 
+               piezas[7].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+        if(piezas[2] != null && piezas[5] != null && piezas[8] != null) 
+        {  
+            if(piezas[2].GetComponent<Pieza>().estado == pieza && 
+               piezas[5].GetComponent<Pieza>().estado == pieza && 
+               piezas[8].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+        
+        // Diagonales
+        if(piezas[0] != null && piezas[4] != null && piezas[8] != null) 
+        {  
+            if(piezas[0].GetComponent<Pieza>().estado == pieza && 
+               piezas[4].GetComponent<Pieza>().estado == pieza && 
+               piezas[8].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+        if(piezas[2] != null && piezas[4] != null && piezas[6] != null) 
+        {  
+            if(piezas[2].GetComponent<Pieza>().estado == pieza && 
+               piezas[4].GetComponent<Pieza>().estado == pieza && 
+               piezas[6].GetComponent<Pieza>().estado == pieza)
+            { 
+                return true;
+            }
+        }
+        return false;
+    }
+
+        /*
+        if(estado[0] == pieza && estado[1] == pieza && estado[2] == pieza) return true; 
+        if(estado[3] == pieza && estado[4] == pieza && estado[5] == pieza) return true; 
+        if(estado[6] == pieza && estado[7] == pieza && estado[8] == pieza) return true; 
+
+        // Líneas verticales
+        if(estado[0] == pieza && estado[3] == pieza && estado[6] == pieza) return true; 
+        if(estado[1] == pieza && estado[4] == pieza && estado[7] == pieza) return true; 
+        if(estado[2] == pieza && estado[5] == pieza && estado[8] == pieza) return true; 
+      
+        // Diagonal
+        if(estado[0] == pieza && estado[4] == pieza && estado[8] == pieza) return true; 
+        if(estado[2] == pieza && estado[4] == pieza && estado[6] == pieza) return true; 
+        */
 
     void dummy() 
     { 
