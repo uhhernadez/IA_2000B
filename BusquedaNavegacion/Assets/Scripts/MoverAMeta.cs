683d8206f119c8dcf0dc40d9fa09609707a90bb6
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-11-03 06:04:15 -0600

    Navegación

---
+++ BusquedaNavegacion/Assets/Scripts/MoverAMeta.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoverAMeta : MonoBehaviour
+{
+    public Transform meta;
+    public NavMeshAgent agente;
+
+    private void Update()
+    {
+           agente.SetDestination(meta.position);
+    }
+
+
+
+}
