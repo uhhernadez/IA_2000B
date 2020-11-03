f40a3ba109b811306ee42f7aebca6c9c00796a56
Uriel Hernandez <uh.hernandez@ugto.mx>
2020-11-03 17:04:58 -0600

    Ejemplo de navegación usando multiples agentes

--- BusquedaNavegacion/Assets/Scripts/MoverAMeta.cs
+++ BusquedaNavegacion/Assets/Scripts/MoverAMeta.cs
@@ -1,18 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class MoverAMeta : MonoBehaviour
 {
     public Transform meta;
     public NavMeshAgent agente;
+    public Text tDistancia;
 
     private void Update()
     {
-           agente.SetDestination(meta.position);
+        NavMeshPathStatus status = agente.pathStatus;
+       
+        //tDistancia.text = "Distancia a la meta: " + agente.remainingDistance;
+        tDistancia.text = "Distancia a la meta: " +Vector3.Distance(agente.pathEndPosition, transform.position);
+        //Debug.Log("Estatus ruta: " +status.ToString());
+        agente.SetDestination(meta.position);
     }
 
-
+    public void Stop(bool comando)
+    {
+        agente.isStopped = comando;
+    }
 
 }
