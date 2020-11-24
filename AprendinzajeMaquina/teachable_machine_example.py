bec6b4ef98f59543a666a9d689035f968ed233ab
Uriel Haile Hernández Belmonte <uh.hernandez@ugto.mx>
2020-11-24 14:15:09 -0600

    Machine learning models

--- AprendinzajeMaquina/teachable_machine_example.py
+++ AprendinzajeMaquina/teachable_machine_example.py
@@ -1,27 +1,44 @@
+import tensorflow as tf
 import tensorflow.keras
 from PIL import Image, ImageOps
 import numpy as np
+import cv2
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
+
 
 # Disable scientific notation for clarity
 np.set_printoptions(suppress=True)
 
 # Load the model
-model = tensorflow.keras.models.load_model('models/keras/01/keras_model.h5')
+model = tensorflow.keras.models.load_model('models/keras/02/keras_model.h5')
+vid = cv2.VideoCapture(0)
 
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+while(True): 
+    ret, frame = vid.read() 
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+    prediction = model.predict(data)
+    print(prediction)
+    cv2.imshow('frame', frame) 
+    if cv2.waitKey(1) & 0xFF == ord('q'): 
+        break
 
-image = Image.open('test_photo.jpg')
-
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-image_array = np.asarray(image)
-
-image.show()
-
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-data[0] = normalized_image_array
-
-prediction = model.predict(data)
-print(prediction)
+vid.release() 
+cv2.destroyAllWindows() 
